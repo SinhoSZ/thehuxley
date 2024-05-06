b2c0fef926779cfcd0c79dbadadb4bf8ef9fd669
SinhoSZ <jailsonjosedc@gmail.com>
2024-05-06 15:10:32 -0300

    questão de if, elif e else

---
+++ code/huxley2226.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+if num == 1:
+    print("Life Mushroom")
+elif num == 2:
+    print("Fire Flower")
+elif num == 3:
+    print("Boost Star")
+elif num == 4:
+    print("Mega Mushroom")
