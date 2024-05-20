95ecf15670fe2acf41f8d709d17a43f38086fba4
SinhoSZ <jailsonjosedc@gmail.com>
2024-05-20 09:03:57 -0300

    add code

---
+++ code/huxley915.py
@@ -0,0 +1,12 @@
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+
+soma = (nota1 + nota2 + nota3 +nota4)/4
+
+print("Informe a primeira nota do aluno:")
+print("Informe a segunda nota do aluno:")
+print("Informe a terceira nota do aluno:")
+print("Informe a quarta nota do aluno:")
+print(f"Media: {soma:.3f}")
