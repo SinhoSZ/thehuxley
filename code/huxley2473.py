96394299161254758f133ff899d9322a755b805b
SinhoSZ <jailsonjosedc@gmail.com>
2024-05-20 10:24:15 -0300

    add codes

---
+++ code/huxley2473.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = int(input())
+
+subtração = a - b
+print(subtração)
---
+++ code/huxley858.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+
+soma = a + b
+print("Digite um numero:")
+print("Digite outro numero:")
+print(f"Soma: {soma:.1f}")
