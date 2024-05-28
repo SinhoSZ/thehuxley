c64534cdfcf72517fa3779478d3fc3a40f1c714c
SinhoSZ <jailsonjosedc@gmail.com>
2024-05-28 14:23:15 -0300

    add code

--- code/huxley3198.py
+++ code/huxley3198.py
@@ -1,6 +1,24 @@
+def divisao(num1, num2):
+        calculo = num1 / num2
+        return calculo
+
+def formatar_resultado(resultado):
+    if resultado.is_integer():
+        return f"{resultado:.1f}"
+    else:
+        resultado_str = f"{resultado:.10f}".rstrip('0')
+        parte_decimal = resultado_str.split('.')[1]
+        if len(parte_decimal) == 1:
+            return f"{resultado:.1f}" # Se a parte decimal tiver apenas um dígito, formata com uma casa decimal 
+        elif len(parte_decimal) == 2:
+            return f"{resultado:.2f}" # Se a parte decimal tiver dois dígitos, formata com duas casas decimais
+        else:
+            return f"{resultado:.2f}" # Se a parte decimal tiver mais de dois dígitos, formata com duas casas decimais
+
 num1 = int(input())
 num2 = int(input())
 
-divisao = num1 / num2
+resultado = divisao(num1, num2)
 
-print(f"{divisao:.1f}" if divisao.is_integer() else f"{divisao:.2f}")
+if resultado is not None:
+    print(formatar_resultado(resultado))
