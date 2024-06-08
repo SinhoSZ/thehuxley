b2c87b61be4442ace21dc5f222338ec38c197cf4
SinhoSZ <jailsonjosedc@gmail.com>
2024-06-08 20:40:12 -0300

    add code

---
+++ code/huxley4240.py
@@ -0,0 +1,10 @@
+# Lê os dois ângulos fornecidos pelo usuário
+angulo1 = int(input())
+angulo2 = int(input())
+    
+
+angulo3 = (180 - (angulo1 + angulo2))
+
+print("Informe o angulo 1 ")
+print("Informe o angulo 2 ") 
+print(f"O terceiro angulo = {angulo3:.3f}")
