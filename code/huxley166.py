c6d8f82038368be083a250cf6b4b33600fe1c1ee
SinhoSZ <jailsonjosedc@gmail.com>
2024-07-19 10:10:48 -0300

    add code

---
+++ code/huxley166.py
@@ -0,0 +1,13 @@
+num = int(input())
+
+if num < 0: # Verifica se o número é negativo
+    numero_revertido_str = str(num)[1:][::-1] # Reverte a parte numérica, sem o sinal negativo
+    # Obter o primeiro dígito e adicionar o sinal negativo
+    primeiro_digito = '-' + numero_revertido_str[0]
+else:
+    # Reverte o número normalmente
+    numero_revertido_str = str(num)[::-1]
+    # Obter o primeiro dígito
+    primeiro_digito = numero_revertido_str[0]
+
+print(primeiro_digito)
