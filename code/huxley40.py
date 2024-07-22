8d121b79e9c2c5764acc969fd91560cd56a7aa88
SinhoSZ <jailsonjosedc@gmail.com>
2024-07-22 14:53:13 -0300

    add code

---
+++ code/huxley40.py
@@ -0,0 +1,3 @@
+valor = float(input())
+garcon = (valor * 0.10) + valor
+print(f"{garcon:.2f}")
---
+++ code/huxley509.py
@@ -0,0 +1,34 @@
+x = int(input())
+y = int(input())
+
+# 0 = falso
+# 1= verdadeiro
+if x > y: # x é maior que y
+    print(1)
+else:
+    print(0)
+
+if x == y: # x é igual a y
+    print(1)
+else:
+    print(0)
+
+if x < y: # x é menor que y
+    print(1)
+else:
+    print(0)
+
+if x != y: # x é diferente de y
+    print(1)
+else:
+    print(0)
+
+if x >= y: # x é maior ou igual a y
+    print(1)
+else:
+    print(0)
+
+if x <= y: # x é menor ou igual a y
+    print(1)
+else:
+    print(0)
