87a5e7319f2f36cd3bdca10ed39d678c0f743f8e
SinhoSZ <jailsonjosedc@gmail.com>
2024-12-01 12:41:33 -0300

    add codigo

---
+++ code/huxley849.py
@@ -0,0 +1,12 @@
+raio1 = float(input())
+raio2 = float(input())
+raio3 = float(input())
+
+pi = 3.1416
+volume1 = (((4 * pi) *  (raio1 ** 3)) / 3)
+volume2 = (((4 * pi) *  (raio2 ** 3)) / 3)
+volume3 = (((4 * pi) *  (raio3 ** 3)) / 3)
+
+print(f'{volume1:.2f}')
+print(f'{volume2:.2f}')
+print(f'{volume3:.2f}')
